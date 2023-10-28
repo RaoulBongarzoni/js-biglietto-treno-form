0a053cb2afe1f5a554ae39b78dc68db19238c205
RaoulBongarzoni <raoul.bongarzoni99@gmail.com>
2023-10-28 17:10:38 +0200

    top section and input

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 let my_name=document.querySelector("#input1");
 let distance=document.querySelector("#input2");
 let btn=document.querySelector("#my_button");
+let eta= document.querySelector("#my_eta");
 
 
 btn.addEventListener("click",function()
 {
 
-    alert("sda" + my_name.value  + distance.value  );
+    alert("sda" + my_name.value  + distance.value + eta.value );
 })
