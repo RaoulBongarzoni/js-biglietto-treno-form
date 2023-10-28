508f66b394acaa12dcdd1348e59f633a2adc0d9a
RaoulBongarzoni <raoul.bongarzoni99@gmail.com>
2023-10-28 17:16:03 +0200

    price calculation

--- js/main.js
+++ js/main.js
@@ -2,10 +2,20 @@ let my_name=document.querySelector("#input1");
 let distance=document.querySelector("#input2");
 let btn=document.querySelector("#my_button");
 let eta= document.querySelector("#my_eta");
-
+const price= 0.21;
+let final_price;
 
 btn.addEventListener("click",function()
 {
 
-    alert("sda" + my_name.value  + distance.value + eta.value );
+    
 })
+
+final_price= Math.round(((distance * price) + Number.EPSILON) * 100) /100;
+
+if(parseInt(eta) < 18){
+    final_price=Math.round(((final_price - (final_price * 20 / 100)) + Number.EPSILON) * 100) /100;
+}
+if(parseInt(eta) >= 65){
+    final_price=Math.round(((final_price - (final_price * 40 / 100)) + Number.EPSILON) * 100) /100;
+}
