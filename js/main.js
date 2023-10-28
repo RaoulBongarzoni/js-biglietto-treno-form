c2d448507815ad888ae1fb629c149571f620195d
RaoulBongarzoni <raoul.bongarzoni99@gmail.com>
2023-10-28 16:17:22 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let my_name=document.querySelector("#input1");
+let distance=document.querySelector("#input2");
+let btn=document.querySelector("#my_button");
+
+
+btn.addEventListener("click",function()
+{
+
+    alert("sda" + my_name.value  + distance.value  );
+})
