84adca1f61cee22175cabdd8292f4b0aae527c42
RaoulBongarzoni <raoul.bongarzoni99@gmail.com>
2023-10-29 17:32:00 +0100

    ticket generation

--- js/main.js
+++ js/main.js
@@ -3,19 +3,28 @@ let distance=document.querySelector("#input2");
 let btn=document.querySelector("#my_button");
 let eta= document.querySelector("#my_eta");
 const price= 0.21;
-let final_price;
+let final_price = 0;
 
 btn.addEventListener("click",function()
-{
+{   
+    distance.value = parseInt(distance.value); 
+    final_price= parseInt(price * distance.value); 
+    
+    if (eta.value=="Minorenne"){
+        final_price= final_price - (final_price * 0.20)
+    }
+    if (eta.value=="Senior"){
+        final_price= final_price - (final_price * 0.40)
+    }
 
     
+    alert(final_price + "   " + distance.value + price + "€");
+
+
+/*     document.getElementById("no-biglietto").classList.add("d-block") */
+    document.getElementById("no-biglietto").classList.add("d-none");
+/*     document.getElementById("biglietto").classList.add("d-block"); */
+    document.getElementById("biglietto").classList.remove("d-none");
+    
+    
 })
-
-final_price= Math.round(((distance * price) + Number.EPSILON) * 100) /100;
-
-if(eta.value == "Minorenne"){
-    final_price=Math.round(((final_price - (final_price * 20 / 100)) + Number.EPSILON) * 100) /100;
-}
-if(eta.value == "senior"){
-    final_price=Math.round(((final_price - (final_price * 40 / 100)) + Number.EPSILON) * 100) /100;
-}
