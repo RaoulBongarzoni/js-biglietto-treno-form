06eedebd3fcb816c86fe3ac3259002d3b4f5e4d5
RaoulBongarzoni <raoul.bongarzoni99@gmail.com>
2023-10-29 17:53:24 +0100

    Complete Ticket Generation

--- js/main.js
+++ js/main.js
@@ -1,24 +1,35 @@
 let my_name=document.querySelector("#input1");
 let distance=document.querySelector("#input2");
-let btn=document.querySelector("#my_button");
+let btn1=document.querySelector("#my_button");
+let btn2=document.querySelector("#my_clear");
 let eta= document.querySelector("#my_eta");
 const price= 0.21;
 let final_price = 0;
 
-btn.addEventListener("click",function()
+btn1.addEventListener("click",function()
 {   
+    
     distance.value = parseInt(distance.value); 
     final_price= parseInt(price * distance.value); 
     
-    if (eta.value=="Minorenne"){
-        final_price= final_price - (final_price * 0.20)
+    if (eta.value=="Under-18"){
+        final_price= final_price - (final_price * 0.20);
+        
     }
     if (eta.value=="Senior"){
-        final_price= final_price - (final_price * 0.40)
+        final_price= final_price - (final_price * 0.40);
+        
     }
 
+    carrozza = Math.round(Math.random() * 10);
+    Cp= Math.round(Math.random() * 100000);
     
-    alert(final_price + "   " + distance.value + price + "€");
+    document.getElementById("NomePasseggero").innerHTML = my_name.value;
+    document.getElementById("sconto").innerHTML = `Biglietto ${eta.value}`;
+    
+    document.getElementById("random10").innerHTML = carrozza;
+    document.getElementById("random5").innerHTML = Cp;
+    document.getElementById("costo").innerHTML = final_price + " €";
 
 
 /*     document.getElementById("no-biglietto").classList.add("d-block") */
@@ -28,3 +39,16 @@ btn.addEventListener("click",function()
     
     
 })
+
+btn2.addEventListener("click", function(){
+
+    
+    /*     document.getElementById("no-biglietto").classList.add("d-block") */
+    document.getElementById("no-biglietto").classList.remove("d-none");
+    /*     document.getElementById("biglietto").classList.add("d-block"); */
+    document.getElementById("biglietto").classList.add("d-none");
+    
+}
+
+
+)
